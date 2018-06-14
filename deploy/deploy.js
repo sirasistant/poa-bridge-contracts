12887257ed4f673bc5f9e294d098adbd7df718e5
Roman Storm <rstormsf@gmail.com>
2018-06-13 21:40:11 -0700

    add deployment scripts

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,18 +1,20 @@
 const fs = require('fs');
 
-const deployHome = require('./src/home');
-const deployForeign = require('./src/foreign');
+const deployHome = require('./src/erc20toErc20/home')
+const deployForeign = require('./src/erc20toErc20/foreign');
 
 async function main() {
-  const homeBridge = await deployHome()
+  const {homeBridgeAddress, erc677tokenAddress, deployedBlockNumber} = await deployHome()
   const {foreignBridge, erc677} = await deployForeign();
   console.log("\nDeployment has been completed.\n\n")
-  console.log(`[   Home  ] HomeBridge: ${homeBridge.address} at block ${homeBridge.deployedBlockNumber}`)
+  console.log(`[   Home  ] HomeBridge: ${homeBridgeAddress} at block ${deployedBlockNumber}`)
   console.log(`[ Foreign ] ForeignBridge: ${foreignBridge.address} at block ${foreignBridge.deployedBlockNumber}`)
-  console.log(`[ Foreign ] POA20: ${erc677.address}`)
+  console.log(`[ Foreign ] SomeERC20 Token: ${erc677.address}`)
+  console.log(`[ Home ] ERC677 Bridgeble Token: ${erc677tokenAddress}`)
   fs.writeFileSync('./bridgeDeploymentResults.json', JSON.stringify({
     homeBridge: {
-      ...homeBridge,
+      homeBridgeAddress,
+      erc677tokenAddress
     },foreignBridge: {
       ...foreignBridge,
     },erc677
