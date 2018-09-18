568303a036f27728ea510191941bbf072e68a799
sirasistant <sirasistant@gmail.com>
2018-09-18 17:35:28 +0200

    Fix the script

--- run-migrations.sh
+++ run-migrations.sh
@@ -1,10 +1,15 @@
 #! /bin/sh
+echo "ERC20 to ERC20 in sidechain"
 rm -r ./build
+rm migrations.log
 npx truffle migrate --reset --network=sidechain -f 3 --to 3 >> migrations.log
+echo "ETH to ERC20 in sidechain"
 rm -r ./build
 npx truffle migrate --reset --network=sidechain -f 4 --to 4 >> migrations.log
+echo "ERC20 to ERC20 in rinkeby"
 rm -r ./build
 npx truffle migrate --reset --network=rinkeby -f 3 --to 3 >> migrations.log
+echo "ETH to ERC20 in rinkeby"
 rm -r ./build
-npx truffle migrate --reset --network=sidechain -f 4 --to 4 >> migrations.log
+npx truffle migrate --reset --network=rinkeby -f 4 --to 4 >> migrations.log
 rm -r ./build
